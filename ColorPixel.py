d74e967ae90f90139efd3327c4a1394df3e74031
Darien <darien.yeung@gmail.com>
2017-05-01 13:42:02 -0700

    Updated ColorPixel

--- ColorPixel.py
+++ ColorPixel.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python
 
-import cv2
-try:
-    Filename = "VID_20170208_144233.avi"
-    video = cv2.VideoCapture(-1)
-    print video.grab()
-except:
-    print "No such file"
+import skvideo.io
+import skvideo.datasets
 
-success,frame = video.read()
-count = 0
-success = True
-while success:
-    success,frame = video.read()
-    #cv2.rectangle(frame, )
-    print int(frame[300,300])
+Filename = ""
+#Filename = skvideo.datasets.bigbuckbunny()
+video = skvideo.io.vread(Filename)
+
+colorArr = []
+for frame in video:
+    tempArr = frame[300,300]
+    temp2 = []
+    temp2.append(float(tempArr[0])/256.0)
+    temp2.append(float(tempArr[1])/256.0)
+    temp2.append(float(tempArr[2])/256.0)
+    colorArr.append(temp2)
+
+
+import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.pyplot as plt
+
+cmap1 = LinearSegmentedColormap.from_list("cmap", colorArr, N=256, gamma=1.0)
+gradient = np.linspace(0,1,256)
+gradient = np.vstack((gradient, gradient))
+plt.imshow(gradient, aspect='auto', cmap=cmap1)
+plt.savefig("GRAD")
+
+
+
