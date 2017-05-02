72c04423e01fffa420903bb65ac623ba0fe4f859
Darien <darien.yeung@gmail.com>
2017-05-02 11:52:47 -0700

    Updated to use Imageio to better support frame extraction

--- ColorPixel.py
+++ ColorPixel.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python
 
-import skvideo.io
-import skvideo.datasets
-
-Filename = ""
-#Filename = skvideo.datasets.bigbuckbunny()
-video = skvideo.io.vread(Filename)
-
-colorArr = []
-for frame in video:
-    tempArr = frame[300,300]
-    temp2 = []
-    temp2.append(float(tempArr[0])/256.0)
-    temp2.append(float(tempArr[1])/256.0)
-    temp2.append(float(tempArr[2])/256.0)
-    colorArr.append(temp2)
-
-
+import imageio
 import numpy as np
 from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.pyplot as plt
+
+import sys
+
+Filename = "/home/darien/LolRoflCopter/ColorPixel/Data/VID2.mp4"
+video = imageio.get_reader(Filename, 'ffmpeg')
+colorArr = []
+
+maxLen = int(len(video))
+
+try:
+    print "Starting Collection"
+    for i, frame in enumerate(video):
+        #tempArr = frame[len(frame)/2,len(frame[0])/2]
+        tempArr = frame[900,366]
+        temp2 = []
+        temp2.append(float(tempArr[0])/256.0)
+        temp2.append(float(tempArr[1])/256.0)
+        temp2.append(float(tempArr[2])/256.0)
+        colorArr.append(temp2)
+
+        sys.stdout.write("\rFrame %i of %i" % (i, maxLen))
+        sys.stdout.flush()
+except KeyboardInterrupt:
+    print "\nEnding Collection" 
 
 cmap1 = LinearSegmentedColormap.from_list("cmap", colorArr, N=256, gamma=1.0)
 gradient = np.linspace(0,1,256)
 gradient = np.vstack((gradient, gradient))
 plt.imshow(gradient, aspect='auto', cmap=cmap1)
 plt.savefig("GRAD")
-
-
 
---
+++ README.md
@@ -0,0 +1,26 @@
+#ColorPixel 
+
+##Darien Yeung (@Yeungdb)
+University of Victoria, McIndoe Group
+
+##Background
+
+ColorPixel is a lightweight Python application for making colorbars from collecting single pixel from an input video. Leveraging imageio's capabilities for video frame extraction and obtaining the RGB values from the individual frame, the colors are stitched together via matplotlib to construct a colorbar plot. 
+
+Developed for the original intent of monitoring the color change in a chemical reaction with a smart phone camera, ColorPixel is able to plot the color change over time as a colormetric chronograph (or colorbar) as per discussed in this paper: 
+
+   {INSERT LINK TO TITANOCENE PAPER}
+
+##Quickstart
+For Linux Users:
+
+`pip install requirements.txt`
+`python ColorPixel.py -f [InputVideoFile]`
+
+##Dependencies
+
+ - Imageio
+ - Matplotlib
+
+##Contact
+
